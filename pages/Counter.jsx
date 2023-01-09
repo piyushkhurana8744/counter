ecd5e257aa05a4d2cabac54d131faa19fdc6bacf
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2023-01-09 10:26:24 +0530

    count

--- pages/Counter.jsx
+++ pages/Counter.jsx
@@ -7,6 +7,7 @@ const Counter = () => {
       <h1>Count :{count}</h1>
       <button disabled={count==1} onClick={()=>setCount(count-1)}>Dec</button>
       <button onClick={()=>setCount(count+1)}>Inc</button>
+      <h1>count</h1>
     </div>
   )
 }
