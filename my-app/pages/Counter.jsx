dced44d860f555a092cdb0cb8fd7cddfbf6a8848
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2023-01-09 09:31:39 +0530

    counter

---
+++ my-app/pages/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count,setCount]=useState(0)
+  return (
+    <div>
+      <h1>Count :{count}</h1>
+      <button disabled={count==1} onClick={()=>setCount(count-1)}>Dec</button>
+      <button onClick={()=>setCount(count+1)}>Inc</button>
+    </div>
+  )
+}
+
+export default Counter
